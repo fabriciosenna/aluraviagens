eaad730a2d29c345479738d301949295e914d6b5
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-23 17:39:13 -0300

    Iniciando a implementação do app Alura Viagens - Módulo 2: IOS Layout

---
+++ Alura Viagens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Fabricio Rodrigo Baixo on 12/23/20.
+//  Copyright © 2020 Fabricio Rodrigo Baixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
