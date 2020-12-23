e66034cb7faf0a1080eb4f87e6941b320f4d4380
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-23 18:00:40 -0300

    implementando table view

--- Alura Viagens/ViewController.swift
+++ Alura Viagens/ViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource {
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    let listaViagem: [String] = ["Rio de Janeiro", "Uberlândia", "São Paulo", "Florianópolis"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tabelaViagens.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagem.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listaViagem[indexPath.row]
+        
+        return cell
+    }
+    
+
 
 
 }
