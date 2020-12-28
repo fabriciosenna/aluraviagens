0b892994e98b3d03fade9d9b8c87023f8f1bd004
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-28 17:38:45 -0300

    customizando a tela de pacotes de viagens

---
+++ Alura Viagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PacotesViagensViewController.swift
+//  Alura Viagens
+//
+//  Created by Fabricio Rodrigo Baixo on 12/28/20.
+//  Copyright © 2020 Fabricio Rodrigo Baixo. All rights reserved.
+//
+
+import UIKit
+
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource {
+
+
+    @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colecaoPacotesViagem.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
+        celulaPacote.backgroundColor = UIColor.blue
+        
+        return celulaPacote
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Alura Viagens/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  Alura Viagens
+//
+//  Created by Fabricio Rodrigo Baixo on 12/28/20.
+//  Copyright © 2020 Fabricio Rodrigo Baixo. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    
+}
