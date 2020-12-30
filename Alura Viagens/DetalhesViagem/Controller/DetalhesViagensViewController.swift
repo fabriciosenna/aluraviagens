eb02136d9f14841019a0a1d43a4f302fd35c6864
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-30 10:06:25 -0300

    Enviando e recebendo objeto entre controllers

--- Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
+++ Alura Viagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -10,21 +10,32 @@ import UIKit
 
 class DetalhesViagensViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var imagemPacoteViagem: UIImageView!
+    @IBOutlet weak var labelTituloPacoteViagem: UILabel!
+    @IBOutlet weak var labelDescricaoPacoteViagem: UILabel!
+    @IBOutlet weak var labelDataViagem: UILabel!
+    @IBOutlet weak var labelPrecoPacoteViagem: UILabel!
+    
+    // MARK: Constantes e Variáveis
+    
+    var pacoteSelecionado:PacoteViagem? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let pacote = pacoteSelecionado else {return print("Erro ao buscar informações sobre o pacoteViagens(classe: DetalhesViagensViewController)")}
+        
+        self.imagemPacoteViagem.image = UIImage(named: pacote.viagem.caminhoDaImagem)
+        self.labelTituloPacoteViagem.text = pacote.viagem.titulo
+        self.labelDescricaoPacoteViagem.text = pacote.descricao
+        self.labelDataViagem.text = pacote.dataViagem
+        self.labelPrecoPacoteViagem.text = pacote.viagem.preco
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func botaoVoltar(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
